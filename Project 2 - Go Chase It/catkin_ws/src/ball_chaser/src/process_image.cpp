516931d0d472114935e920efc0ce89387def63d3
Tamzin Walker <tamzinwalker@gmail.com>
2019-12-03 17:34:44 +0800

    developed process_image_callback

--- Project 2 - Go Chase It/catkin_ws/src/ball_chaser/src/process_image.cpp
+++ Project 2 - Go Chase It/catkin_ws/src/ball_chaser/src/process_image.cpp
@@ -9,6 +9,12 @@ ros::ServiceClient client;
 void drive_robot(float lin_x, float ang_z)
 {
     // TODO: Request a service and pass the velocities to it to drive the robot
+    ROS_INFO_STREAM("Passing velocities to drive robot");
+
+    ball_chaser::DriveToTarget srv;
+    srv.request.linear_x = lin_x;
+    srv.request.angular_z = ang_z;
+
 }
 
 // This callback function continuously executes and reads the image data
@@ -16,11 +22,45 @@ void process_image_callback(const sensor_msgs::Image img)
 {
 
     int white_pixel = 255;
+    int move_left = 0;
+    int move_mid = 0;
+    int move_right = 0;
 
     // TODO: Loop through each pixel in the image and check if there's a bright white one
     // Then, identify if this pixel falls in the left, mid, or right side of the image
     // Depending on the white ball position, call the drive_bot function and pass velocities to it
     // Request a stop when there's no white ball seen by the camera
+
+    // Loop through each pixel in image and check if there is a bright white one
+    for (int i = 0: i < img.height * img.step; i++)
+    {
+        // Identify if the pixel falls in the left, mid, or right side of image
+        if (img.data[i] == img.data[white_pixel])
+        {
+            // pixel left of image
+            if (img.height[i] <= (1/3)*img.height) 
+            {
+                move_left = 1;
+                // call drive_bot function
+            }
+
+            // pixel mid of image
+            if ((1/3)*img.height < img.height[i] <= (2/3)*img.height) 
+            {
+                move_mid = 1;
+                // call drive_bot function
+            }
+
+            // pixel right of image
+            if ((2/3)*img.height < img.height[i]) 
+            {
+                move_right = 1;
+                // call drive_bot function
+            }
+        }
+        // Request a stop when there is no white ball seen by the camera
+    }
+
 }
 
 int main(int argc, char** argv)
